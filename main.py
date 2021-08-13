af33bbd5795d20eda24430d17e17207d9488813f
toshi1014 <toshihide1014go@gmail.com>
2021-08-13 18:18:29 +0900

    add: main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from handle_image import HandleImage
+from read_contents import ReadContents
+
+img_path = "calender.png"
+min_length = 40
+row, column = [6, 7]
+handle_image = ReadContents(img_path, min_length, row, column)
